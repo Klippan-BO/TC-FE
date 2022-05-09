759a7de86788bbbf4780fc847df0deaf5ac48c06
Matthew Pak <mattpak@gmail.com>
2022-05-08 18:15:11 -0700

    map bounds listener updated to only update on dragend or zoom change to prevent the updater from running continually while dragging the map

--- components/maps/mapLogic.js
+++ components/maps/mapLogic.js
@@ -11,31 +11,37 @@ const getBounds = (map, maps) => {
 
 // Can update to expand based on given array of places for location search
 
+const updateQuad = (map, maps, trails) => {
+  const {
+    north, south, east, west,
+  } = getBounds(map, maps);
+  const lngCenter = (east + west) / 2;
+  const latCenter = (north + south) / 2;
+  trails.forEach((trail) => {
+    const { lat, lng } = trail;
+    switch (true) {
+      case (lat > latCenter && lng <= lngCenter):
+        trail.quadrant = 1;
+        break;
+      case (lat > latCenter && lng > lngCenter):
+        trail.quadrant = 2;
+        break;
+      case (lat <= latCenter && lng > lngCenter):
+        trail.quadrant = 3;
+        break;
+      default: // in quad 4
+        trail.quadrant = 4;
+    }
+  });
+};
+
 const mapQuadrants = (map, maps, trails) => {
-  // Probably needs modulator to prevent continuous reevaluation of marker quadrant status
-  map.addListener('bounds_changed', () => {
-    console.log('CHANGING BOUNDS');
-    const {
-      north, south, east, west,
-    } = getBounds(map, maps);
-    const lngCenter = (east + west) / 2;
-    const latCenter = (north + south) / 2;
-    trails.forEach((trail) => {
-      const { lat, lng } = trail;
-      switch (true) {
-        case (lat > latCenter && lng <= lngCenter):
-          trail.quadrant = 1;
-          break;
-        case (lat > latCenter && lng > lngCenter):
-          trail.quadrant = 2;
-          break;
-        case (lat <= latCenter && lng > lngCenter):
-          trail.quadrant = 3;
-          break;
-        default: // in quad 4
-          trail.quadrant = 4;
-      }
-    });
+  updateQuad(map, maps, trails);
+  map.addListener('dragend', () => {
+    updateQuad(map, maps, trails);
+  });
+  map.addListener('zoom_changed', () => {
+    updateQuad(map, maps, trails);
   });
 };
 
