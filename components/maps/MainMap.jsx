a5429ecf30f50697979951ccb57685593d5515b3
Lawrence Malloy <lcmalloy@gmail.com>
2022-05-09 10:06:01 -0700

    implemented getGeo functionality

--- components/maps/MainMap.jsx
+++ components/maps/MainMap.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import axios from 'axios';
 import GoogleMapReact from 'google-map-react';
-
+import { Search, GpsFixed } from '@material-ui/icons';
 import Marker from './Marker';
 import MAPSAPIKEY from '../../config';
-
 import dummyTrails from './dummyData';
 
 const containerStyle = {
@@ -16,13 +16,74 @@ const center = {
   lng: -122.41,
 };
 
+const mapAutoApi = 'https://maps.googleapis.com/maps/api/js';
+
+function loadAsyncScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement('script');
+    Object.assign(script, {
+      type: 'text/javascript',
+      async: true,
+      src,
+    });
+    script.addEventListener('load', () => resolve(script));
+  });
+}
 // PASS "lat" + "lng" PROPS TO MARKER
 
+const getGeo = () => {
+  const location = '2361 Glenview St Alameda CA';
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+    params: {
+      address: location,
+      key: 'AIzaSyCEblyOtkFC7QgjoP3bZa7FFz-a3-NCsJA',
+    },
+  })
+    .then((res) => {
+      console.log(res.data.results[0].geometry.location);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 export default function MainMap() {
+  const searchInput = useRef(null);
+  const initMapScript = () => {
+    if (window.google) {
+      return Promise.resolve();
+    }
+    const src = `${mapAutoApi}?key=${MAPSAPIKEY.MAPSAUTOKEY}&libraries=places&v=weekly`;
+    return loadAsyncScript(src);
+  };
+
+  const onChangeAddress = (autocomplete) => {
+    const location = autocomplete.getPlace();
+    console.log(location);
+  };
+  const initAutocomplete = () => {
+    if (!searchInput.current) return;
+
+    const autocomplete = new window.google.maps.places.Autocomplete(searchInput.current);
+    autocomplete.setFields(['address_component', 'geometry']);
+    autocomplete.addListener('place_changed', () => onChangeAddress(autocomplete));
+  };
+
+  useEffect(() => {
+    initMapScript().then(() => {
+      console.log('map loaded', window.google);
+    });
+  }, []);
+
+  initAutocomplete();
+  getGeo();
   return (
     <div style={containerStyle}>
+      <div className="search">
+        <input ref={searchInput} type="text" placeholder="Search location..." />
+      </div>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: MAPSAPIKEY }}
+        bootstrapURLKeys={{ key: MAPSAPIKEY.MAPSAPIKEY }}
         defaultCenter={center}
         defaultZoom={10}
         yesIWantToUseGoogleMapApiInternals
---
+++ components/maps/PlaceAutocorrect.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PlaceAutocorrect() {
+  return (
+    <script async
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5A45TH2-reTyjpGtaO8xio07p01RU8Ac&libraries=places&callback=initMap">
+    </script>
+  );
+}
+
+export default PlaceAutocorrect;
