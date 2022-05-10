c49d0923ee340413a6110bb3725adbcb57ad2b2c
RphPandan <pascal9057@gmail.com>
2022-05-10 10:05:42 -0700

    added image gallery, buttons are set up to send upvotes to database

--- components/Trails/TrailCarousel.jsx
+++ components/Trails/TrailCarousel.jsx
@@ -10,10 +10,11 @@ import Stack from '@mui/material/Stack';
 import StarRateIcon from '@mui/icons-material/StarRate';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import BorderAllIcon from '@mui/icons-material/BorderAll';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import axios from 'axios';
+import Box from '@mui/material/Box';
+import ImageGallery from './ImageGallery';
 import Typography from '@mui/material/Typography';
 
 const sample_data = require('./sampleData');
@@ -58,14 +59,17 @@ const modalPictureStyle = {
 };
 
 function TrailCarousel() {
-  const [photos] = useState(sample_data.trail1.photos);
+  const [photos] = useState(sample_data.trail1.photos); // is this sorted by upvotes already?
   const [index, setIndex] = useState(0);
   const [interval, setInterval] = useState(false);
   const [open, setOpen] = useState(false);
   const [photoModal, setPhotoModal] = useState(false);
   const [imageSelected, setImageSelected] = useState('');
+  const [imageGallery, setImageGallery] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const galleryOpen = () => setImageGallery(true);
+  const galleryClose = () => setImageGallery(false);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
@@ -88,6 +92,7 @@ function TrailCarousel() {
       .then((result) => {
         // console.log(JSON.parse(result.request.response).url);
         // <-- cloudinary link to post to database
+        // need trail id and then can send to the database and post
       })
       .catch((err) => {
         console.log(err);
@@ -116,6 +121,7 @@ function TrailCarousel() {
       >
         <Button>
           <BorderAllIcon
+            onClick={galleryOpen}
             sx={iconStyles}
           />
         </Button>
@@ -185,7 +191,6 @@ function TrailCarousel() {
                     width: '100%',
                     height: '60vh',
                     objectFit: 'cover',
-                    backgroundColor: 'yellow',
                     filter: 'blur(8px)',
                   }}
                 />
@@ -237,6 +242,16 @@ function TrailCarousel() {
             submit photo
           </Button>
         </Box>
+      </Modal>
+      <Modal
+        open={imageGallery}
+        onClose={galleryClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{
+        }}
+      >
+        <ImageGallery photos={photos} />
       </Modal>
     </Box>
   );
